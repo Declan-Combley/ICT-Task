706ac58de31220b4339d5c66b48cc7d5431a03de
Declan Combley <90402120+Declan-Combley@users.noreply.github.com>
2024-05-23 08:53:11 +0800

    Update game3.js

--- scripts/game3.js
+++ scripts/game3.js
@@ -1,11 +1,29 @@
-
 const cards = ['diamond', 'spade', 'heart', 'club'];
 
 const playerCards = document.querySelector('.userCard');
 const computerCards = document.querySelector('.compCard');
 
 function play () {
+    let dealtCards = []; 
+    let currentCard; 
     
+    while (currentCard !== 'spade') {
+        let randomCardIndex = Math.floor(Math.random() * 4);
+        currentCard = cards[randomCardIndex];
+        dealtCards.push(currentCard);
+    }
+    
+    dealtCards.push('WINNER', 'LOSER');
+    
+    for (let i = 0; i < dealtCards.length; i++) {
+        if (i % 2 === 0) {
+            playerCards.textContent += dealtCards[i] + ' ';
+        } else {
+            computerCards.textContent += dealtCards[i] + ' ';   
+        }
+    }
+    
+    endGame();
 }
 
 function endGame() {
